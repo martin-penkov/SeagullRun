410f92f0209ecb0610d5630ca40ca647de494367
martin_penkov <mirander73@gmail.com>
2021-10-19 00:56:54 +0300

    implemented movement, player animation and sprite added (logic needs arrangement in folders and files by their purpose)

--- src/entry.js
+++ src/entry.js
@@ -1,3 +1,18 @@
+const offset = window.innerHeight / 3;
+const upperPos = 0;
+const midPos = offset;
+const lowerPos = offset * 2;
+const posArray = [lowerPos, midPos, upperPos]
+let currentPosValue = 1
+
+console.log(`Widow height is: ${window.innerHeight}`)
+
+console.log(upperPos)
+console.log(midPos);
+console.log(lowerPos);
+
+let player;
+
 // The application will create a renderer using WebGL, if possible,
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container.
@@ -5,23 +20,77 @@ const app = new PIXI.Application({
     width: window.innerWidth,
     height: window.innerHeight,
     transparent: false,
-    antialias: true
+    antialias: false
 });
 //remove default chrome styling
 app.renderer.view.style.position = 'absolute';
+
+
+//keyboard input event listener attach to dom
+document.addEventListener('keydown', onKeyDown);
 
 
 
 const loader = new PIXI.Loader();
 document.body.appendChild(app.view);
 
-loader.add('tileset', './sprites/player/seagull.json').load(setup);
+loader.add('./sprites/player/seagull.json').load(setup);
 
 function setup(loader, resources) {
- const playerTexture = PIXI.Texture.from('tile000.png');
- const playerSprite = new PIXI.Sprite(playerTexture);
+//  const playerTexture = PIXI.Texture.from('tile000.png');
+//  const playerSprite = new PIXI.Sprite(playerTexture);
 
- playerSprite.position.set(100, 100);
- playerSprite.scale.set(2, 2);
- app.stage.addChild(playerSprite);
+
+
+    let sheet = loader.resources["./sprites/player/seagull.json"];
+    //pattern for images <name>_<number>.png
+    const textureArr = [];
+    for (let i = 0; i < 8; i++)
+    {
+        const texture = PIXI.Texture.from(`tile_00${i}.png`);
+        textureArr.push(texture);
+    }
+
+    player = new PIXI.AnimatedSprite(textureArr);
+
+    player.position.set(100, posArray[currentPosValue]);
+    player.anchor.x = 1;
+    player.scale.set(-3, 3);
+    app.stage.addChild(player);
+    player.play();
+    player.animationSpeed = 0.15;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function onKeyDown(key) {
+    // W Key is 87
+    // Up arrow is 38
+    if (key.keyCode === 87 || key.keyCode === 38) {
+        if(currentPosValue < 2){
+            currentPosValue++;
+        }
+        player.position.y = posArray[currentPosValue];
+    }
+
+    // S Key is 83
+    // Down arrow is 40
+    if (key.keyCode === 83 || key.keyCode === 40) {
+        if(currentPosValue > 0){
+            currentPosValue--;
+        }
+        player.position.y = posArray[currentPosValue];
+    }
 }
